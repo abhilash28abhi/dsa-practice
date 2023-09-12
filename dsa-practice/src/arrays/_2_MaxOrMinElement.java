46fb548b6a69819c391f3f72bb1ffddf16c5b6c2
Abhilash <abhilash@ge.com>
2023-09-12 08:49:57 +0530

    second prog

--- dsa-practice/src/arrays/_2_MaxOrMinElement.java
+++ dsa-practice/src/arrays/_2_MaxOrMinElement.java
@@ -12,6 +12,8 @@ public class _2_MaxOrMinElement {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         findMaxMin(arr);
+        System.out.println("Min element using recursion is : " + findMinUsingRecursion(arr, 0 , arr.length));
+        System.out.println("Max element using recursion is : " + findMaxUsingRecursion(arr, 0 , arr.length));
     }
 
     //O(NlogN)
@@ -19,5 +21,17 @@ public class _2_MaxOrMinElement {
         Arrays.sort(arr);
         System.out.println("Min element is : " + arr[0]);
         System.out.println("Max element is : " + arr[arr.length - 1]);
+    }
+
+    // O(N) TC
+    // O(N) SC since recursion will be using the stack
+    private static int findMinUsingRecursion (int[] arr, int i , int n) {
+        //i is the index starting from 0
+        return n == 1 ? arr[i] : Math.min(arr[i], findMinUsingRecursion(arr, i + 1, n - 1));
+    }
+
+    private static int findMaxUsingRecursion (int[] arr, int i , int n) {
+        //i is the index starting from 0
+        return n == 1 ? arr[i] : Math.max(arr[i], findMaxUsingRecursion(arr, i + 1, n - 1));
     }
 }
